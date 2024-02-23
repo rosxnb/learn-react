bed6b50d4acd39b6fd5403a9723aa0cd53d62582
Roshan Bharati <rosxnb@gmail.com>
2024-02-23 11:23:48 +0545

    example: movie components

---
+++ 07-use-popcorn/src/StarRating.js
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+const styleContainer = {
+    display: "flex",
+    alignItems: "center",
+    gap: "16px",
+};
+
+const styleStarContainer = {
+    display: "flex",
+};
+
+StarRating.prototype = {
+    maxRating: PropTypes.number,
+    color: PropTypes.string,
+    size: PropTypes.number,
+    onSetRating: PropTypes.func,
+    defaultRating: PropTypes.number,
+    className: PropTypes.string,
+};
+
+export default function StarRating({ maxRating=5, color="#fcc419", size=48, className="", onSetRating, defaultRating=0 }) {
+    const styleText = {
+        lineHeight: "1",
+        margin: "0",
+        color,
+        fontSize: `${size / 1.5}px`,
+    };
+
+    const [rating, setRating] = useState(defaultRating);
+    const [hoverRating, setHoverRating] = useState(0);
+
+    const handleRating = (rating) => {
+        setRating(rating);
+        onSetRating(rating);
+    }
+    const handleHoverRating = (rating) => setHoverRating(rating)
+
+    return (
+        <div style={styleContainer} className={className}>
+            <div style={styleStarContainer}>
+                {Array.from({ length: maxRating }, (_, i) => (
+                    <span key={i}>
+                        <Star
+                            onClick={() => handleRating(i + 1)}
+                            onHover={() => handleHoverRating(i + 1)}
+                            onNotHover={() => handleHoverRating(0)}
+                            isFilled={hoverRating ? hoverRating >= i + 1 : rating >= i + 1}
+                        />
+                    </span>
+                ))}
+            </div>
+            <p style={styleText}>
+                {rating || ""}
+            </p>
+        </div>
+    );
+}
+
+function Star({ onClick, onHover, onNotHover, isFilled, color = "#fcc419", size = 48 }) {
+    const styleStar = {
+        display: "block",
+        height: `${size}px`,
+        width: `${size}px`,
+        cursor: "pointer",
+        color,
+    };
+
+    return (
+        <span role="button" style={styleStar} onClick={onClick} onMouseEnter={onHover} onMouseLeave={onNotHover}>
+            {isFilled
+                ? <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill={color}
+                    stroke={color}
+                >
+                    <path
+                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                    />
+                </svg>
+                : <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke={color}
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="{2}"
+                        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                    />
+                </svg>
+            }
+        </span>
+    );
+}
